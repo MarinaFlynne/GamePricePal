28b16ec50f5470e7b785ad10a9c47cd0de661caf
Marina <marinaflynne@gmail.com>
2024-01-04 18:23:48 -0800

    Add Game class

---
+++ Game DB Tool/Game.cs
@@ -0,0 +1,19 @@
+﻿namespace Game_DB_Tool;
+
+public class Game
+{
+    private string title;
+    private string id;
+    private string type;
+    private bool mature;
+    private string releaseDate;
+
+    public Game(string title, string id, string type, bool mature, string releaseDate)
+    {
+        this.title = title;
+        this.id = id;
+        this.type = type;
+        this.mature = mature;
+        this.releaseDate = releaseDate;
+    }
+}
